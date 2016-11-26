37297b2cce0825c3ee756f919e3cbabc09425b29
zhuzhuxia <zhuzhuxia@zhuzhuxiadeMacBook-Pro.local>
2016-11-26 12:12:21 +0800

    update

---
+++ demo/immutable/index.js
@@ -0,0 +1,29 @@
+var Immutable = require('immutable');
+var map1 = Immutable.Map({a:1, b:2, c:3});
+var map2 = map1.set('b', 50);
+
+console.log(map1.get('b')); // 2
+console.log(map2.get('b')); // 50
+
+
+var nested = Immutable.fromJS({a:{b:{c:[3,4,5]}}});
+// Map { a: Map { b: Map { c: List [ 3, 4, 5 ] } } }
+var nested2 = nested.mergeDeep({a:{b:{d:6}}});
+// Map { a: Map { b: Map { c: List [ 3, 4, 5 ], d: 6 } } }
+
+console.log(nested2)
+
+
+nested2.getIn(['a', 'b', 'd']); // 6
+
+
+console.log(nested2.getIn(['a', 'b', 'd']))
+
+
+var nested3 = nested2.updateIn(['a', 'b', 'd'], value => value + 1);
+// Map { a: Map { b: Map { c: List [ 3, 4, 5 ], d: 7 } } }
+
+var nested4 = nested3.updateIn(['a', 'b', 'c'], list => list.push(6));
+// Map { a: Map { b: Map { c: List [ 3, 4, 5, 6 ], d: 7 } } }
+
+console.log(nested4)
